9b8f3c5a8295194ac36e2d8020006fec02ec1871
aquimo <aquimoadriano@gmail.com>
2023-07-08 14:15:13 +0200

    Atualização

---
+++ src/principal/motoristas.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Aquimo
+ */
+public class motoristas {
+   private String nome;
+   private String apelido;
+   private Date dataNascimento;
+   private int telefone;
+   private String tipoDeCartaConducao;
+   private String sexo;
+   private int NUIT;
+   private String numeroCarta;
+   private int id;
+    
+    
+    
+    public String getNome(){
+        return nome;
+    }
+    public String getapelido(){
+        return apelido;
+    }
+    public Date getDataNascimento(){
+        return dataNascimento;
+  
